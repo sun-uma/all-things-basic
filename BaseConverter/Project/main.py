a7421306edf56c46f17780ea104f9700cb9603e2
umasun <umasun12@gmail.com>
2021-09-05 20:23:47 +0530

    initial commit

---
+++ BaseConverter/Project/main.py
@@ -0,0 +1,61 @@
+import math
+import string
+import sys
+
+
+def integer(n: int, b: int) -> string:
+    if n < b:
+        if n > 9:
+            return str(mapping[n])
+        else:
+            return str(n)
+    mod = n % b
+    if mod > 9:
+        mod = mapping[mod]
+    return integer(n // b, b) + str(mod)
+
+
+def fraction(n: float, b: int) -> string:
+    if n == 0:
+        return '0'
+    n = n * b
+    i = math.floor(n)
+    d = n - i
+    if i > 9:
+        i = mapping[i]
+    return str(i) + fraction(d, b)
+
+
+def print_result (neg: bool, int_value: int, fract: float, base: int):
+    result = ''
+    if neg:
+        result += '-'
+    result += integer(int_value, base)
+    if not fract == 0:
+        result += '.' + fraction(fract, base)
+    print(result)
+
+
+try:
+    n = float(sys.argv[1])
+    mapping = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+    neg = False
+    if n < 0:
+        neg = True
+    n = abs(n)
+    int_value = math.floor(n)
+    fract = n - int_value
+    print('Binary: ', end="")
+    print_result(neg, int_value, fract, 2)
+    print('Octal: ', end="")
+    print_result(neg, int_value, fract, 8)
+    print('Hexadecimal: ', end="")
+    print_result(neg, int_value, fract, 16)
+
+except ValueError as v:
+    print("Invalid values!")
+    print(v)
+
+except IndexError as i:
+    print("Arguments not found!")
+    print(i)
---
+++ BaseConverter/README.md
@@ -0,0 +1,9 @@
+Base Converter is a program that takes input as a decimal number and prints the binary, octal and hexadecimal equivalent. It takes numbers with fractional parts as well.
+
+It is written in Python 3.8 and developed in PyCharm. It imports sys, math and string libraries. The time complexity is O(log n) for whole numbers where n is the decimal number. It is O(n) for fractional numbers where n is the length of the output fractional part.
+
+To run the program:
+
+Clone the repository or download the Project folder on your computer. Ensure you have Python 3.8 installed on your system. Navigate to the folder containing the program on terminal. Execute using the command 'python Project/main.py <decimal number>'.
+
+Thank your for using Base Converter!
